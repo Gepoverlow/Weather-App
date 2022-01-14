0230b0fe355ce3af5a63245d8a1641fdd46d3c00
Gepoverlow <gepeoverload@gmail.com>
2022-01-14 16:16:16 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const country = document.getElementById("country");
+const weather = document.getElementById("weather");
+const temperature = document.getElementById("temperature");
+const searchBar = document.getElementById("search-bar");
+const searchButton = document.getElementById("search-button");
+
+let url;
+
+async function getWeather() {
+  try {
+    const response = await fetch(url, { mode: "cors" });
+    const weatherData = await response.json();
+
+    country.textContent = weatherData.name;
+    weather.textContent = weatherData.weather[0].main;
+    temperature.textContent = `${weatherData.main.temp} Cº`;
+    console.log(weatherData);
+  } catch (error) {
+    country.textContent = "Country not found";
+    console.log(error);
+  }
+}
+
+searchButton.addEventListener("click", () => {
+  url = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&units=metric&APPID=6c679bb6ad2f56b93b4591bc560920e8`;
+
+  getWeather();
+});
+
+function degreeConverter() {}
